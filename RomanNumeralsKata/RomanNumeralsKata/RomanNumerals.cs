9632855174574b29aaec149030d19ebe4b76a027
Daniel Shwarcman <dshwarcman@gmail.com>
2013-05-14 16:40:57 +0300

    RED; tests 1 to 8

--- RomanNumeralsKata/RomanNumeralsKata/RomanNumerals.cs
+++ RomanNumeralsKata/RomanNumeralsKata/RomanNumerals.cs
@@ -10,7 +10,13 @@ namespace RomanNumeralsKata
     {
         public string oneToThree(int num)
         {
-            if (num == 1)
+            if (num > 8)
+                return "X";
+            else if (num > 4)
+                return "V" + oneToThree(num - 4);
+            else if (num == 4)
+                return "IV";
+            else if (num == 1)
                 return "I";
             else if (num == 2)
                 return "II";
--- RomanNumeralsKata/RomanNumeralsKataTests/UnitTest1.cs
+++ RomanNumeralsKata/RomanNumeralsKataTests/UnitTest1.cs
@@ -8,18 +8,21 @@ namespace RomanNumeralsKataTests
     public class UnitTest1
     {        
         [TestMethod]
-        RomanNumerals roman = new RomanNumerals();
         public void return1to3Tests()
         {
+            RomanNumerals roman = new RomanNumerals();
             Assert.AreEqual("I", roman.oneToThree(1));
             Assert.AreEqual("II", roman.oneToThree(2));
             Assert.AreEqual("III", roman.oneToThree(3));
         }
+
+        [TestMethod]
         public void return4to8Tests()
         {
+            RomanNumerals roman = new RomanNumerals();
             Assert.AreEqual("IV", roman.oneToThree(4));
             Assert.AreEqual("V", roman.oneToThree(5));
-            Assert.AreEqual("VI", roman.oneToThree(6));
+            Assert.AreEqual("VI", roman.oneToThree(9));
             Assert.AreEqual("VII", roman.oneToThree(7));
             Assert.AreEqual("VIII", roman.oneToThree(8));
         }
