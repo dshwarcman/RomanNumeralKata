84d289eec81ffd2565b844c998cb1c046f6260d0
Daniel Shwarcman <dshwarcman@gmail.com>
2013-05-14 16:15:51 +0300

    GREEN; return 1 to 3 tests

--- RomanNumeralsKata/RomanNumeralsKataTests/UnitTest1.cs
+++ RomanNumeralsKata/RomanNumeralsKataTests/UnitTest1.cs
@@ -11,8 +11,8 @@ namespace RomanNumeralsKataTests
         public void return1to3Tests()
         {
             RomanNumerals roman = new RomanNumerals();
-            Assert.AreEqual("I", roman.oneToThree(2));
-            Assert.AreEqual("II", roman.oneToThree(1));
+            Assert.AreEqual("I", roman.oneToThree(1));
+            Assert.AreEqual("II", roman.oneToThree(2));
             Assert.AreEqual("III", roman.oneToThree(3));
         }
     }
