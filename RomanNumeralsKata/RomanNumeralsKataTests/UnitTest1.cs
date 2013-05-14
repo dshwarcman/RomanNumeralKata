adae99afb93e9df723f3d06ac2944a4e61b79a9a
Daniel Shwarcman <dshwarcman@gmail.com>
2013-05-14 16:28:37 +0300

    BLUE; tests 1 to 8

--- RomanNumeralsKata/RomanNumeralsKataTests/UnitTest1.cs
+++ RomanNumeralsKata/RomanNumeralsKataTests/UnitTest1.cs
@@ -8,12 +8,20 @@ namespace RomanNumeralsKataTests
     public class UnitTest1
     {        
         [TestMethod]
+        RomanNumerals roman = new RomanNumerals();
         public void return1to3Tests()
         {
-            RomanNumerals roman = new RomanNumerals();
             Assert.AreEqual("I", roman.oneToThree(1));
             Assert.AreEqual("II", roman.oneToThree(2));
             Assert.AreEqual("III", roman.oneToThree(3));
+        }
+        public void return4to8Tests()
+        {
+            Assert.AreEqual("IV", roman.oneToThree(4));
+            Assert.AreEqual("V", roman.oneToThree(5));
+            Assert.AreEqual("VI", roman.oneToThree(6));
+            Assert.AreEqual("VII", roman.oneToThree(7));
+            Assert.AreEqual("VIII", roman.oneToThree(8));
         }
     }
 }
